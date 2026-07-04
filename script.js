ee35d857071b91d985489ea508a9acbf9156c228
karata9 <karatalrafden2024@gmail.com>
2026-07-04 21:47:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js"; 
 import { getFirestore, collection, addDoc, getDocs, updateDoc, doc, deleteDoc as fsDeleteDoc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 const firebaseConfig = {
